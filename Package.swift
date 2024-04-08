98c97d1972126126d402107c7213d79e446272ac
ASS <ckfmchk7sq@privaterelay.appleid.com>
2024-04-08 14:20:48 +0900

    PrivacyManifest対応

--- Package.swift
+++ Package.swift
@@ -10,15 +10,19 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LaunchKit",
-            targets: ["LaunchKit"]),
+            targets: ["LaunchKit"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LaunchKit"),
+            name: "LaunchKit",
+            resources: [.copy("PrivacyInfo.xcprivacy")]
+        ),
         .testTarget(
             name: "LaunchKitTests",
-            dependencies: ["LaunchKit"]),
+            dependencies: ["LaunchKit"]
+        ),
     ]
 )
